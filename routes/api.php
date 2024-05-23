1147ea8bfd8c27fa4ad19a8c96a8f055c9b1e928
Matheus Rosmaninho <matheusrosmano@gmail.com>
2024-05-23 02:06:20 +0000

    Cria rotas de banco e getAll

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\V1\BankController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['prefix'=> 'v1'], function () {
+    Route::apiResource('banks', BankController::class);
+});
