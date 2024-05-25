0978d47f79a69aa8812b1138d25b0e9cb2342860
Matheus Rosmaninho <matheusrosmano@gmail.com>
2024-05-25 13:23:25 +0000

    Cria register e login de api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\V1\AuthController;
 use App\Http\Controllers\V1\BankController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,6 @@ Route::get('/user', function (Request $request) {
 Route::group(['prefix'=> 'v1'], function () {
     Route::apiResource('banks', BankController::class);
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
